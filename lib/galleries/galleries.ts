84bb3a9cd74bec691b01db159692dbcace9370b7
FelipeGardenghiDev <felipegardenghi@outlook.com>
2026-09-02 17:04:19 -0300

    feat(galleries): connect official Gist raw endpoint for show photo albums

--- lib/galleries/galleries.ts
+++ lib/galleries/galleries.ts
@@ -1,9 +1,9 @@
 import type { EventGallery } from '@/types/site';
 import { eventGalleries as fallbackGalleries } from '@/data/galleries';
 
-// Substitua esta URL pelo link RAW do seu Gist com as galerias (ex: .../raw/galerias.json)
+// URL oficial RAW do Gist com as informações das galerias de shows
 const GALLERIES_GIST_URL =
-  'https://gist.githubusercontent.com/FelipeGardenghiDev/galerias/raw/galerias.json';
+  'https://gist.githubusercontent.com/FelipeGardenghiDev/b50cfd1d73d576115a1909cbcbdc1573/raw';
 
 export interface GalleriesApiResponse {
   galerias?: EventGallery[];
